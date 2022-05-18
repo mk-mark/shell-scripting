069c1c63e180fe114e741baede52c9e297bbb70c
mohan <markmk0796@gmail.com>
2022-05-18 17:49:50 +0530

    dns

--- create-ec2.sh
+++ create-ec2.sh
@@ -8,8 +8,6 @@ fi
 COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g' )
-
-
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-from-public | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g' )
 
 ZONE_ID="Z0403776LH8X8XMWVJBK"
@@ -20,7 +18,7 @@ PRIVATE_IP=$(aws ec2 run-instances \
   --instance-type t2.micro \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
   --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-  --security-group-ids ${SGID} | jq '.Instances[].PrivateIpAddress' )
+  --security-group-ids ${SGID} | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 sed -e "s/IPADDRESS/${PRIVATE_IP}" -e "s/COMPONENT/${COMPONENT}" route53.json >/tmp/record.json
 
