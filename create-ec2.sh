9b4256b4587eaf89fb502d6642e0ae2ac13b5ca9
mohan <markmk0796@gmail.com>
2022-05-18 17:39:26 +0530

    rolling back

--- create-ec2.sh
+++ create-ec2.sh
@@ -15,13 +15,13 @@ SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-a
 ZONE_ID="Z0403776LH8X8XMWVJBK"
 
 echo $AMI_ID
-PRIVATE_IP=${aws ec2 run-instances \
+PRIVATE_IP=$(aws ec2 run-instances \
   --image-id ${AMI_ID} \
   --instance-type t2.micro \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
   --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-  --security-group-ids ${SGID} \
-  | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'}
+  --security-group-ids ${SGID}
+  | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 
 
