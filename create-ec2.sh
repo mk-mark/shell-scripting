d9eab3c19caca3240f68f6cc466dfd9ecd3d0c49
mohan <markmk0796@gmail.com>
2022-05-18 18:19:37 +0530

    Auto ec2 and DNS

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,34 +1,36 @@
+#!/bin/bash
 
-
-if [ -z "$1" ] ; then
+if [ -z "$1" ]; then
   echo -e "\e[31mInput Machine Name is needed\e[0m"
   exit 1
 fi
 
 COMPONENT=$1
+ZONE_ID="Z08005452NVNO9YZAOL1R"
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g' )
-SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-from-public | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g' )
+create_ec2() {
+  PRIVATE_IP=$(aws ec2 run-instances \
+      --image-id ${AMI_ID} \
+      --instance-type t2.micro \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      --security-group-ids ${SGID} \
+      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-ZONE_ID="Z0403776LH8X8XMWVJBK"
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
 
-echo $AMI_ID
-PRIVATE_IP=$(aws ec2 run-instances \
-  --image-id ${AMI_ID} \
-  --instance-type t2.micro \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
-  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-  --security-group-ids ${SGID} | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-from-public | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
-sed -e "s/IPADDRESS/${PRIVATE_IP}" -e "s/COMPONENT/${COMPONENT}" route53.json >/tmp/record.json
-
-aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
-
-
-
-
-
-
-
+if [ "$1" == "all" ]; then
+  for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis ; do
+    COMPONENT=$component
+    create_ec2
+  done
+else
+  create_ec2
+fi
 
 
