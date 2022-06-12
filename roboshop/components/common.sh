baa26d3035d2a213add6942896e9cfd3ea7bd6d0
mohan <markmk0796@gmail.com>
2022-06-12 12:49:50 +0530

    Updating shell-scripting/

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -52,9 +52,9 @@ SERVICE_SETUP() {
           -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
           -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
           -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
-          -e 's/CARTENDPOINT/b_cart.roboshop.internal/' \
+          -e 's/CARTENDPOINT/cart.roboshop.internal/' \
           -e 's/DBHOST/mysql.roboshop.internal/' \
-          -e 's/CARTHOST/b_cart.roboshop.internal/' \
+          -e 's/CARTHOST/cart.roboshop.internal/' \
           -e 's/USERHOST/user.roboshop.internal/' \
           -e 's/AMQPHOST/rabbitmq.roboshop.internal/' \
           /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service  &>>${LOG_FILE}
